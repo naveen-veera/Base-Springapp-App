2d0da38cd788ef806a7210412c1b494ff3693994
naveen-veera <veeranaveen.sethuraman@gmail.com>
2021-10-10 19:02:48 +0530

    Final Commit

---
+++ src/main/java/com/oracle/OracleTesting/OracleTestingApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.OracleTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oracle/OracleTesting/OracleTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.OracleTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
